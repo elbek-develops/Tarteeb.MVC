e44d1ae4f579c2731634d78ccbe8efc59e47e3a5
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-11-07 00:20:45 -0700

    Complete CRUD operations

--- Tarteeb.MVC/Controllers/TaskController.cs
+++ Tarteeb.MVC/Controllers/TaskController.cs
@@ -6,13 +6,24 @@ using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Tarteeb.MVC.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace Tarteeb.MVC.Controllers
 {
     public class TaskController : Controller
     {
-        private const string baseUrl = "http://localhost:5000/";
 
+        private readonly ILogger<TaskController> logger;
+        private readonly IConfiguration configuration;
+        private readonly string baseTasksUrl;
+
+        public TaskController(ILogger<TaskController> logger, IConfiguration configuration)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.configuration = configuration;
+            baseTasksUrl = configuration.GetSection("HostNames").GetSection("TasksHost").Value;
+        }
 
         public async Task<IActionResult> Index()
         {
@@ -21,7 +32,7 @@ namespace Tarteeb.MVC.Controllers
             using (var client = new HttpClient())
             {
                 //Passing service base url
-                client.BaseAddress = new Uri(baseUrl);
+                client.BaseAddress = new Uri(baseTasksUrl);
                 client.DefaultRequestHeaders.Clear();
                 //Define request data format
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -37,6 +48,114 @@ namespace Tarteeb.MVC.Controllers
                 }
                 //returning the TaskViewModel list to view
                 return View(tasks);
+            }
+        }
+
+        // GET: TaskController/Create
+        public ActionResult Create()
+        {
+            ViewData["Title"] = "Create";
+            return View();
+        }
+
+        // POST: taskController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TaskViewModel taskViewModel)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string url = baseTasksUrl + "api/Tasks";
+                    var postTask = await client.PostAsJsonAsync<TaskViewModel>(url, taskViewModel);
+                    if (!postTask.IsSuccessStatusCode)
+                    {
+                        return View();
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Error occured on create task:" + ex.Message);
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // GET: taskController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            TaskViewModel task = new TaskViewModel();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                string endpoint = "api/Tasks/" + id;
+                client.BaseAddress = new Uri(baseTasksUrl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync(endpoint);
+                if (response.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var taskResponse = await response.Content.ReadAsStringAsync();
+                    //Deserializing the response recieved from web api and storing into  the TaskViewModel list
+                    task = JsonConvert.DeserializeObject<TaskViewModel>(taskResponse);
+                }
+                //returning the TaskViewModel list to view
+                ViewData["Title"] = "Update";
+                return View("Create", task);
+            }
+        }
+
+        // PUT: taskController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, TaskViewModel taskViewModel)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string url = baseTasksUrl + "api/Tasks";
+                    var putTask = await client.PutAsJsonAsync<TaskViewModel>(url, taskViewModel);
+                    if (!putTask.IsSuccessStatusCode)
+                    {
+                        return View();
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Error occured on update task:" + ex.Message);
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // DELETE: TaskController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string url = baseTasksUrl + "api/Tasks/" + id;
+                    var deleteTask = await client.DeleteAsync(url);
+                    if (!deleteTask.IsSuccessStatusCode)
+                    {
+                        logger.LogError("Error occured on delete task.");
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch
+            {
+                logger.LogError("Error occured on delete task.");
+                return RedirectToAction(nameof(Index));
             }
         }
     }
--- Tarteeb.MVC/Controllers/TimeController.cs
+++ Tarteeb.MVC/Controllers/TimeController.cs
@@ -6,22 +6,33 @@ using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Tarteeb.MVC.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace Tarteeb.MVC.Controllers
 {
     public class TimeController : Controller
     {
-        private const string baseUrl = "http://localhost:5050/";
+        private readonly string baseTimesUrl;
+        private readonly string baseTasksUrl;
+        private readonly ILogger<TimeController> logger;
+        private readonly IConfiguration configuration;
 
+        public TimeController(ILogger<TimeController> logger, IConfiguration configuration)
+        {
+            this.logger = logger;
+            this.configuration = configuration;
+            baseTasksUrl = configuration.GetSection("HostNames").GetSection("TasksHost").Value;
+            baseTimesUrl = configuration.GetSection("HostNames").GetSection("TimesHost").Value;
+        }
 
         public async Task<IActionResult> Index()
         {
-            //Hosted web API REST Service base url
             List<TimeViewModel> times = new List<TimeViewModel>();
             using (var client = new HttpClient())
             {
                 //Passing service base url
-                client.BaseAddress = new Uri(baseUrl);
+                client.BaseAddress = new Uri(baseTimesUrl);
                 client.DefaultRequestHeaders.Clear();
                 //Define request data format
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -37,6 +48,156 @@ namespace Tarteeb.MVC.Controllers
                 }
                 //returning the TimeViewModel list to view
                 return View(times);
+            }
+        }
+
+        // GET: TimeController/Create
+        public async Task<ActionResult> Create()
+        {
+            List<TaskViewModel> tasks = new List<TaskViewModel>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(baseTasksUrl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees  using HttpClient(
+                HttpResponseMessage response = await client.GetAsync("api/Tasks");
+                //Checking the response is successful or not which is sent using
+                if (response.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var tasksResponse = response.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response recieved from web api and storing into  the TaskViewModel list
+                    tasks = JsonConvert.DeserializeObject<List<TaskViewModel>>(tasksResponse);
+                }
+                ViewBag.Tasks = tasks;
+                ViewData["Title"] = "Create";
+            }
+            return View();
+        }
+
+        // POST: TimeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TimeViewModel timeViewModel)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string url = baseTimesUrl + "api/Times";
+                    var postTask = await client.PostAsJsonAsync<TimeViewModel>(url, timeViewModel);
+                    if (!postTask.IsSuccessStatusCode)
+                    {
+                        return View();
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Error occured on create time:" + ex.Message);
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // GET: TimeController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            #region Get Tasks
+            List<TaskViewModel> tasks = new List<TaskViewModel>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(baseTasksUrl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees  using HttpClient(
+                HttpResponseMessage response = await client.GetAsync("api/Tasks");
+                //Checking the response is successful or not which is sent using
+                if (response.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var tasksResponse = response.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response recieved from web api and storing into  the TaskViewModel list
+                    tasks = JsonConvert.DeserializeObject<List<TaskViewModel>>(tasksResponse);
+                }
+                ViewBag.Tasks = tasks;
+                ViewData["Title"] = "Update";
+            }
+            #endregion
+            TimeViewModel time = new TimeViewModel();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                string endpoint = "api/Times/" + id;
+                client.BaseAddress = new Uri(baseTimesUrl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync(endpoint);
+                if (response.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var timeResponse = await response.Content.ReadAsStringAsync();
+                    //Deserializing the response recieved from web api and storing into  the TimeViewModel list
+                    time = JsonConvert.DeserializeObject<TimeViewModel>(timeResponse);
+                }
+                //returning the TimeViewModel list to view
+                return View("Create",time);
+            }
+        }
+
+        // PUT: TimeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id,TimeViewModel timeViewModel)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string url = baseTimesUrl + "api/Times";
+                    var putTask = await client.PutAsJsonAsync<TimeViewModel>(url, timeViewModel);
+                    if (!putTask.IsSuccessStatusCode)
+                    {
+                        return View();
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Error occured on update time:" + ex.Message);
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // DELETE: TimeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string url = baseTimesUrl + "api/Times/" + id;
+                    var deleteTask = await client.DeleteAsync(url);
+                    if (!deleteTask.IsSuccessStatusCode)
+                    {
+                        logger.LogError("Error occured on delete time.");
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch
+            {
+                logger.LogError("Error occured on delete time.");
+                return RedirectToAction(nameof(Index));
             }
         }
     }
